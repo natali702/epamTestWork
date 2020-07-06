e7453c949767725060ebf0ea8dceaffa5c367997
natali702 <b.a.d.wolf@yandex.ru>
2020-07-06 14:58:08 +0300

    refactor

---
+++ src/main/java/app/utils/JDBCUtils.java
@@ -0,0 +1,49 @@
+package app.utils;
+
+import java.io.PrintWriter;
+import java.sql.*;
+import java.time.LocalDate;
+
+public class JDBCUtils {
+
+    private static String jdbcURL = "jdbc:postgresql://localhost:5434/java_ee_db";
+    private static String jdbcUsername = "postgres";
+    private static String jdbcPassword = "admin";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void printSQLException(SQLException ex) {
+        for (Throwable e : ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+
+    public static Date getSQLDate(LocalDate date) {
+        return java.sql.Date.valueOf(date);
+    }
+
+    public static LocalDate getUtilDate(Date sqlDate) {
+        return sqlDate.toLocalDate();
+    }
+}
