bc87736297c77d9560dd3368d09f18136209b50e
natali702 <b.a.d.wolf@yandex.ru>
2020-07-06 14:56:18 +0300

    refactor

--- src/main/java/app/dao/Dao.java
+++ src/main/java/app/dao/Dao.java
@@ -1,16 +1,29 @@
 package app.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public interface Dao<T> {
-    Optional<T> get(long id);
+ //   Optional<T> get(long id);
 
-    List<T> getAll();
+  //  List<T> getAll();
 
-    void save(T t);
+   // void save(T t);
 
-    void update(T t, String[]params);
+   // void update(T t, String[]params);
 
-    void delete(T t);
+   // void delete(T t);
+    //
+
+    void insert(T t) throws SQLException;
+
+    T select(long tId);
+
+    List<T> selectAll();
+
+    boolean delete(int id) throws SQLException;
+
+    boolean update(T t) throws SQLException;
+
 }
