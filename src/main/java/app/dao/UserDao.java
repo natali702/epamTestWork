e979cee14906509d12c841f4f857c0b9fa77ff7d
natali702 <b.a.d.wolf@yandex.ru>
2020-07-06 14:56:53 +0300

    refactor

--- src/main/java/app/dao/UserDao.java
+++ src/main/java/app/dao/UserDao.java
@@ -1,11 +1,11 @@
 package app.dao;
 
-import app.entitles.User;
+import app.model.User;
+import app.utils.JDBCUtils;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 public class UserDao {
     private final List<User> users = new ArrayList<>();
@@ -27,7 +27,7 @@ public class UserDao {
         result.setId(-1);
 
         for (User user : users) {
-            if (user.getName().equals(name) && user.getPassword().equals(password)) {
+            if (user.getUsername().equals(name) && user.getPassword().equals(password)) {
                 result = user;
             }
         }
@@ -36,7 +36,7 @@ public class UserDao {
 
     public boolean add(final User user) {
         for (User u : users) {
-            if (u.getName().equals(user.getName()) && u.getPassword().equals(user.getPassword())) {
+            if (u.getUsername().equals(user.getUsername()) && u.getPassword().equals(user.getPassword())) {
                 return false;
             }
         }
@@ -46,11 +46,40 @@ public class UserDao {
     public boolean userIsExist(final String name, final String password) {
         boolean result = false;
         for (User user : users) {
-            if (user.getName().equals(name) && user.getPassword().equals(password)) {
+            if (user.getUsername().equals(name) && user.getPassword().equals(password)) {
                 result = true;
                 break;
             }
         }
         return result;
+    }
+
+    public boolean validate(User user) throws ClassNotFoundException {
+
+        boolean status = false;
+
+        Class.forName("org.postgresql.Driver");
+
+        try (Connection connection = JDBCUtils.getConnection();
+             //PreparedStatement: предварительно компилирует запросы,
+             //которые могут содержать входные параметры
+             PreparedStatement preparedStatement = connection
+                     .prepareStatement("select * from users where username = ? and password = ? ")) {
+            //Устанавливаем в нужную позицию значения определённого типа
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.setString(2, user.getPassword());
+
+            System.out.println(preparedStatement);
+            //выполняем запрос
+            ResultSet rs = preparedStatement.executeQuery();
+            //rs это указатель на первую строку с выборки
+            //чтобы вывести данные мы будем использовать
+            //метод next() , с помощью которого переходим к следующему элементу
+            status = rs.next();
+
+        } catch (SQLException e) {
+            JDBCUtils.printSQLException(e);
+        }
+        return status;
     }
 }
