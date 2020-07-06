923074de9a6d1b982d5a051ee707de6f31cfc189
natali702 <b.a.d.wolf@yandex.ru>
2020-07-06 14:57:06 +0300

    refactor

--- src/main/java/app/filters/AuthenticationFilter.java
+++ src/main/java/app/filters/AuthenticationFilter.java
@@ -1,35 +1,52 @@
 package app.filters;
 
+import app.model.User;
+import app.services.Authentication;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-//@WebFilter(urlPatterns = "/*")
+
+@WebFilter(urlPatterns = "/*")
 public class AuthenticationFilter implements Filter {
+
+    private Authentication authService;
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-//        HttpServletRequest req = (HttpServletRequest) servletRequest;
-//        HttpServletResponse res = (HttpServletResponse) servletResponse;
-//
-//        String path = req.getRequestURI().substring(req.getContextPath().length());
-//        if(path.startsWith("/login") || path.startsWith("/images")){
-//            filterChain.doFilter(req,res);
-//            return;
-//        }
-//        HttpSession session = req.getSession(false);
-//        if(session == null){
-//            req.getRequestDispatcher("/index.html").forward(req,res);
-//        } else {
-//            filterChain.doFilter(req,res);
-//        }
-    }
+        System.out.println("start doFilter from AuthenticationFilter");
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
 
+        String path = req.getRequestURI().substring(req.getContextPath().length());
+        System.out.println(path);
+        if (path.startsWith("/login") || path.startsWith("/images")) {
+            filterChain.doFilter(req, res);
+            return;
+        }
+        HttpSession session = req.getSession(false);
+        boolean isAuth = false;
+
+        if (session != null) {
+            User sessionAttribute = (User) session.getAttribute("user");
+            if (sessionAttribute != null) {
+                isAuth = authService.isUserAuthenticated(sessionAttribute.getUsername(), sessionAttribute.getPassword());
+            }
+        }
+
+        System.out.println("session from doFilter from AuthenticationFilter = " + session);
+        if (session == null || !isAuth) {
+            req.getRequestDispatcher("WEB-INF/views/login.jsp").forward(req, res); //change index.html
+        } else {
+            filterChain.doFilter(req, res);
+        }
+    }
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
+        this.authService = (Authentication) filterConfig.getServletContext().getAttribute("authService");
     }
 
     @Override
