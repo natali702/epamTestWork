4cf43f31435e08f3d27e06cf797e9b401119db66
natali702 <b.a.d.wolf@yandex.ru>
2020-07-07 22:26:25 +0300

    refactor

--- src/main/java/app/dao/UserDao.java
+++ src/main/java/app/dao/UserDao.java
@@ -8,51 +8,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao {
+    private static final String SELECT_USER_BY_NAME_PASSWORD = "select * from users where username = ? and password = ? ";
     private final List<User> users = new ArrayList<>();
 
-    public User getUserById(int id) {
-        User result = new User();
-        result.setId(-1);
-
-        for (User user : users) {
-            if (user.getId() == id) {
-                result = user;
-            }
-        }
-        return result;
-    }
-
-    public User getUserByloginPassword(final String name, final String password) {
-        User result = new User();
-        result.setId(-1);
-
-        for (User user : users) {
-            if (user.getUsername().equals(name) && user.getPassword().equals(password)) {
-                result = user;
-            }
-        }
-        return result;
-    }
-
-    public boolean add(final User user) {
-        for (User u : users) {
-            if (u.getUsername().equals(user.getUsername()) && u.getPassword().equals(user.getPassword())) {
-                return false;
-            }
-        }
-        return users.add(user);
-    }
-
-    public boolean userIsExist(final String name, final String password) {
-        boolean result = false;
-        for (User user : users) {
-            if (user.getUsername().equals(name) && user.getPassword().equals(password)) {
-                result = true;
-                break;
-            }
-        }
-        return result;
-    }
+//    public User getUserById(int id) {
+//        User result = new User();
+//        result.setId(-1);
+//
+//        for (User user : users) {
+//            if (user.getId() == id) {
+//                result = user;
+//            }
+//        }
+//        return result;
+//    }
+//
+//    public User getUserByloginPassword(final String name, final String password) {
+//        User result = new User();
+//        result.setId(-1);
+//
+//        for (User user : users) {
+//            if (user.getUsername().equals(name) && user.getPassword().equals(password)) {
+//                result = user;
+//            }
+//        }
+//        return result;
+//    }
+//
+//    public boolean add(final User user) {
+//        for (User u : users) {
+//            if (u.getUsername().equals(user.getUsername()) && u.getPassword().equals(user.getPassword())) {
+//                return false;
+//            }
+//        }
+//        return users.add(user);
+//    }
+//
+//    public boolean userIsExist(final String name, final String password) {
+//        boolean result = false;
+//        for (User user : users) {
+//            if (user.getUsername().equals(name) && user.getPassword().equals(password)) {
+//                result = true;
+//                break;
+//            }
+//        }
+//        return result;
+//    }
 
     public boolean validate(User user) throws ClassNotFoundException {
 
@@ -64,7 +65,7 @@ public class UserDao {
              //PreparedStatement: предварительно компилирует запросы,
              //которые могут содержать входные параметры
              PreparedStatement preparedStatement = connection
-                     .prepareStatement("select * from users where username = ? and password = ? ")) {
+                     .prepareStatement(SELECT_USER_BY_NAME_PASSWORD)) {
             //Устанавливаем в нужную позицию значения определённого типа
             preparedStatement.setString(1, user.getUsername());
             preparedStatement.setString(2, user.getPassword());
