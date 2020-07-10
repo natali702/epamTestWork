8f87d6dd63b06c9e9497d531348b1e394d7fdf14
natali702 <b.a.d.wolf@yandex.ru>
2020-07-10 13:02:10 +0300

    refactor

--- src/main/java/app/dao/TaskDao.java
+++ src/main/java/app/dao/TaskDao.java
@@ -1,5 +1,6 @@
 package app.dao;
 
+import app.model.Goal;
 import app.model.Task;
 import app.utils.JDBCUtils;
 
@@ -14,9 +15,17 @@ import java.util.List;
 public class TaskDao implements Dao {
     private static final String INSERT_TASK_SQL = "INSERT INTO tasks"
             + "  (title, description, username, task_date,  is_done) VALUES " + " (?, ?, ?, ?, ?);";
-    private static final String SELECT_TASK_BY_ID = "select t_id,title,description,username,task_date,is_done "
-            + "from tasks where t_id =?";
-    private static final String SELECT_ALL_TASKS = "select * from tasks";
+//    private static final String SELECT_TASK_BY_ID = "select t_id,title,description,username,task_date,is_done,goal_id "
+//            + "from tasks where t_id =?";
+    private static final String SELECT_TASK_BY_ID = "SELECT t_id,tasks.title,description,username,task_date,is_done," +
+            "goal_id,g_id, goals.title AS g_title, parent FROM tasks LEFT JOIN goals ON tasks.goal_id = goals.g_id " +
+            "WHERE   t_id =?";
+//    private static final String SELECT_ALL_TASKS = "select * from tasks";
+private static final String SELECT_ALL_TASKS = "SELECT t_id,tasks.title,description,username,task_date,is_done,"
+        + "goal_id,g_id, goals.title AS g_title, parent FROM tasks LEFT JOIN goals ON tasks.goal_id = goals.g_id";
+    private static final String SELECT_ALL_TASKS_BY_USERNAME = "SELECT t_id,tasks.title,description,username,task_date,is_done,"
+            + "goal_id,g_id, goals.title AS g_title, parent FROM tasks LEFT JOIN goals ON tasks.goal_id = goals.g_id " +
+            "WHERE username = ?";
     private static final String UPDATE_TASK = "update tasks set title = ?, description =?, username= ?, task_date =?,"
             + " is_done = ? where t_id = ?;";
     private static final String DELETE_TASK_BY_ID = "delete from tasks where t_id = ?;";
@@ -80,7 +89,14 @@ public class TaskDao implements Dao {
                 String username = rs.getString("username");
                 LocalDate taskDate = rs.getDate("task_date").toLocalDate();
                 boolean isDone = rs.getBoolean("is_done");
-                task = new Task(id, title, description,username,  taskDate, isDone);
+                long goalId =  rs.getLong("goal_id");
+                if(goalId != 0){
+                    Goal goal = new Goal(rs.getLong("g_id"), rs.getString("g_title"),
+                            rs.getLong("parent"));
+                    task = new Task(id, title, description,username,  taskDate, isDone, goalId, goal);
+                }else {
+                    task = new Task(id, title, description,username,  taskDate, isDone, goalId);
+                }
             }
         } catch (SQLException exception) {
             JDBCUtils.printSQLException(exception);
@@ -105,9 +121,52 @@ public class TaskDao implements Dao {
                 String title = rs.getString("title");
                 String username = rs.getString("username");
                 String description = rs.getString("description");
-                LocalDate targetDate = rs.getDate("task_date").toLocalDate();
+                LocalDate taskDate = rs.getDate("task_date").toLocalDate();
                 boolean isDone = rs.getBoolean("is_done");
-                tasks.add(new Task(id, title, description, username, targetDate, isDone));
+                //tasks.add(new Task(id, title, description, username, targetDate, isDone));
+                long goalId =  rs.getLong("goal_id");
+                if(goalId != 0){
+                    Goal goal = new Goal(rs.getLong("g_id"), rs.getString("g_title"),
+                            rs.getLong("parent"));
+                    tasks.add(new Task(id, title, description,username,  taskDate, isDone, goalId, goal));
+                }else {
+                    tasks.add(new Task(id, title, description,username,  taskDate, isDone, goalId));
+                }
+            }
+        } catch (SQLException exception) {
+            JDBCUtils.printSQLException(exception);
+        }
+        return tasks;
+    }
+
+    public List<Task> selectAllTasksByUsername(String name) {
+
+        List<Task> tasks = new ArrayList<>();
+
+        try (Connection connection = JDBCUtils.getConnection();
+
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_TASKS_BY_USERNAME);) {
+            preparedStatement.setString(1, name);
+            System.out.println(preparedStatement);
+            //выполняет команду SELECT. Возвращает объект ResultSet, который содержит результаты запроса
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                long id = rs.getLong("t_id");
+                String title = rs.getString("title");
+                String username = rs.getString("username");
+                String description = rs.getString("description");
+                LocalDate taskDate = rs.getDate("task_date").toLocalDate();
+                boolean isDone = rs.getBoolean("is_done");
+                //tasks.add(new Task(id, title, description, username, targetDate, isDone));
+                long goalId =  rs.getLong("goal_id");
+                if(goalId != 0){
+                    Goal goal = new Goal(rs.getLong("g_id"), rs.getString("g_title"),
+                            rs.getLong("parent"));
+                    tasks.add(new Task(id, title, description,username,  taskDate, isDone, goalId, goal));
+                }else {
+                    tasks.add(new Task(id, title, description,username,  taskDate, isDone, goalId));
+                }
             }
         } catch (SQLException exception) {
             JDBCUtils.printSQLException(exception);
