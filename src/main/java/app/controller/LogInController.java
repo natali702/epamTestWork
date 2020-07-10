5887aed474181f507fecc5475053776bd808c548
natali702 <b.a.d.wolf@yandex.ru>
2020-07-11 00:02:41 +0300

    refactor

--- src/main/java/app/controller/LogInController.java
+++ src/main/java/app/controller/LogInController.java
@@ -19,45 +19,36 @@ public class LogInController extends HttpServlet {
     private UserDao userDao;
 
     public void init() {
-        System.out.println("init LogInController");
         userDao = new UserDao();
         this.authService = (Authentication) getServletContext().getAttribute("authService");
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("doGet LogInController");
-            resp.sendRedirect("WEB-INF/views/login.jsp"); //change index.html
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.sendRedirect("WEB-INF/views/login.jsp");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("doPost LogInController");
         authenticate(req, resp);
     }
 
     private void authenticate(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        System.out.println("start authenticate LogInController");
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         User user = new User();
         user.setUsername(username);
         user.setPassword(password);
-
         try {
             if (userDao.validate(user)) {
                 HttpSession session = request.getSession();
                 session.setAttribute("user", user);
-
                 authService.setUserData(username, password);
-
-               RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/views/task_list.jsp");
-               // RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/views/main_page.jsp");
+                RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/views/task_list.jsp");
                 dispatcher.forward(request, response);
             } else {
-
-                //session.setAttribute("user", username);
-                //response.sendRedirect("login.jsp");
+                RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/views/login.jsp");
+                dispatcher.forward(request, response);
             }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
